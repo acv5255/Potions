b75cc7ae59457fa638929965253592550f8fc596
acv5255 <acv5255@psu.edu>
2023-11-24 12:58:27 -0500

    Updated Doxygen comments on ODE and Optimization libraries

--- my_libs/ode/include/ode.hpp
+++ my_libs/ode/include/ode.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+/** @file */
+
 /*
     Author: Andrew Vierbicher
     This file contains the functions used for numerically solving 
@@ -12,30 +14,48 @@
 
 using arma::Col;
 using std::function;
-using f64 = double;
 
-/*
-    Solve a first-order system of ordinary differential equations using the classical
-    4th-order Runge-Kutta one-step method. Note: this method may not be suitable for stiff
-    systems, though it is much faster than implicit methods.
-*/
+/**
+ * @brief Evolve an ODE using a 4th order explicit Runge-Kutta method
+ * Solve a first-order system of ordinary differential equations using the classical
+ * 4th-order Runge-Kutta one-step method. Note: this method may not be suitable for stiff
+ * systems, though it is much faster than implicit methods.
+ * @tparam T 
+ * @param func The differential equations to evaulate
+ * @param x0 The initial state of the system
+ * @param dt The time step to simulate the model
+ * @return Col<T> 
+ */
 template<typename T>
 Col<T> runge_kutta_explicit(const function<Col<T>(Col<T>)>& func, const Col<T>& x0, T dt);
 
-/*
-    Solve a system of ordinary differential equations using the 4th order Runge-Kutta-Gauss 
-    method. This method is unconditionally stable and will yield a stable solution for stable
-    ODE's, though it is an implicit method and will be slower than explicit methods.
+
+/**
+ * @brief Evolve an ODE system using the implicit 4th order Runge-Kutta-Gauss method
+ * Solve a system of ordinary differential equations using the 4th order Runge-Kutta-Gauss 
+ * method. This method is unconditionally stable and will yield a stable solution for stable
+ * ODE's, though it is an implicit method and will be slower than explicit methods.
+ * @tparam T 
+ * @param func The ODE system
+ * @param x0 The initial state of the system
+ * @param dt The time step
+ * @return Col<T> 
  */
 template<typename T>
 Col<T> runge_kutta_implicit(const function<Col<T>(Col<T>)>& func, const Col<T>& x0, T dt);
 
-/*
-    Solve an ODE over a single time step using either an implicit or explicit method without
-    having to worry about the stiffness. Both the implicit and explicit methods are 4th order
-    consistent. This method will determine the stability of the ODE to determine the best solver. 
-    If the system is stable, the method will solve with the faster Runge-Kutta method, and will
-    use the slower, yet stable, implicit method if the explicit method is not stable.
+/**
+ * @brief Solve an ODE system using an explicit method if stable and an unconditionally stable implicit Runge-Kutta method instead.
+ * Solve an ODE over a single time step using either an implicit or explicit method without
+ * having to worry about the stiffness. Both the implicit and explicit methods are 4th order
+ * consistent. This method will determine the stability of the ODE to determine the best solver. 
+ * If the system is stable, the method will solve with the faster Runge-Kutta method, and will
+ * use the slower, yet stable, implicit method if the explicit method is not stable.
+ * @tparam T 
+ * @param func The ODE system
+ * @param x0 The initial state of the system
+ * @param dt The time step
+ * @return Col<T> 
  */
 template<typename T>
 Col<T> solve_ode(const function<Col<T>(Col<T>)>& func, const Col<T>& x0, T dt);
--- my_libs/optimization/include/optimization.hpp
+++ my_libs/optimization/include/optimization.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+/** @file */
+
 #include <stdexcept>
 #include <functional>
 #include <optional>
@@ -10,11 +12,36 @@ using std::optional;
 using arma::Col;
 using arma::Mat;
 
-const unsigned int MAX_ITERATIONS = 50;
+const unsigned int MAX_ITERATIONS = 50; /** Maximum number of iterations before failing */
 using f64 = double;
 
+/**
+ * @brief Numerically approximate the jacobian matrix for a vector function
+ * Compute the numerical approximation for the Jacobian matrix using the
+ * centered-difference approximation. This requires the size of the function
+ * 'f' to be the number of input parameters so that the corresponding Jacobian
+ * matrix is square.
+ * @tparam T A floating point data type
+ * @param func The continuous function whose Jacobian to approximate
+ * @param x The value at which to approximate the Jacobian of 'f'
+ * @return Mat<T> 
+ */
 template<typename T>
 Mat<T> jacobian(const function<Col<T>(Col<T>)>& func, const Col<T>& x);
 
+/**
+ * @brief Determine the root of a general vector function
+ * Determine the root of a general vector function using Newton-Raphson iteration.
+ * Newton-Raphson iteration requires evaluating a Jacobian and then solving the 
+ * resulting linear system. The iterative method looks like:
+ * x^{i+1} = x^{i} - J(x^{i})^{-1}f(x^{i})
+ * This method has quadratic convergence, though may be unstable and therefore requires
+ * an initial guess that is sufficiently close to the root.
+ * @tparam T 
+ * @param func The function to find the root of
+ * @param jac The function that generates the jacobian matrix
+ * @param x0 The initial guess for the root
+ * @return optional<Col<T>> 
+ */
 template<typename T>
 optional<Col<T>> root(const function<Col<T>(Col<T>)>& func, const function<Mat<T>(Col<T>)>& jac, const Col<T>& x0);
